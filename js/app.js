93d7a98869851cf9c00ba0be37beb3ea13d0f0fe
dale <hessdale96@gmail.com>
2023-03-14 19:29:17 -0600

    using javascript variables

--- js/app.js
+++ js/app.js
@@ -2,7 +2,25 @@ let string_1 = "12";
 let number_1 = 1;
 let string_2 = "23";
 let number_2 = 2;
+let user_points = 66
+
+
 console.log(string_1);
 console.log(number_1);
 console.log(string_2);
 console.log(number_2);
+
+if (number_2 >= 1) {
+    console.log("number 2 is bigger than 1");
+}
+if (string_2 >= 22) {
+    console.log("string 23 is bigger than 22");
+}
+
+if (user_points >= 75) {
+    console.log("more than 75 points!");
+}
+
+if (user_points <= 75) {
+    console.log("not enough points :(");
+}
